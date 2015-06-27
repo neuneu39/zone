48063a09f9906215df887916dab47378b0c38525
kan.akutsu <sakamotoshizuya@gmail.com>
2015-06-27 16:23:51 +0900

    brush up view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
-end
+  #ページごとの完全なタイトル
+  def full_title(page_title)
+    base_title = "ZONE"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+ end
--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module Zone
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-      config.i18n.default_locale = :ja
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.i18n.default_locale = :ja
 
   end
 end
