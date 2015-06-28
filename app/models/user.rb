abf7b787d47a988cf200f6c63bf56d220a89ba9c
kan.akutsu <sakamotoshizuya@gmail.com>
2015-06-28 13:57:55 +0900

    make users_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validate :name, presence: true, length: { maximum: 50 }
+    
+
 end
